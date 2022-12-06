66c5397b41169418bd8d75b3d73959303342f242
whoknowsi <echosmania@gmail.com>
2022-12-06 16:25:49 -0300

    challenge 06

--- challenge06/index.js
+++ challenge06/index.js
@@ -1,14 +1,28 @@
+
+// puntos 160
 function createCube(size) {
-    let cube = ''
-    const H = size * 2
-    for (let i = 0; i < H; i++) {
-        if (i < size) {
-            cube += (' ').repeat(size - i - 1) + '/\\'.repeat(i + 1) + ('_\\').repeat(size) + '\n'
-        } else {
-            cube += (' ').repeat(i - size) + '\\/'.repeat(H - i) + ('_/').repeat(size) + (H === i + 1 ? '' : '\n')
-        }
+    let top = []
+    let bottom = []
+    for (let i = 0; i < size; i++) {
+        top.push((' ').repeat(size - i - 1) + '/\\'.repeat(i + 1) + ('_\\').repeat(size))
+        bottom.push((' ').repeat(i) + '\\/'.repeat(size - i) + ('_/').repeat(size))
+
     }
-    return cube
+    return [top, bottom].flat().join('\n')
 }
+
+// puntos 100
+// function createCube(size) {
+//     let cube = ''
+//     const H = size * 2
+//     for (let i = 0; i < H; i++) {
+//         if (i < size) {
+//             cube += (' ').repeat(size - i - 1) + '/\\'.repeat(i + 1) + ('_\\').repeat(size) + '\n'
+//         } else {
+//             cube += (' ').repeat(i - size) + '\\/'.repeat(H - i) + ('_/').repeat(size) + (H === i + 1 ? '' : '\n')
+//         }
+//     }
+//     return cube
+// }
 
 module.exports = createCube
