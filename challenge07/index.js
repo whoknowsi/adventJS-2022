66dc3a4d6ba3e36a7e3f7ed92dacba94971dd627
whoknowsi <echosmania@gmail.com>
2022-12-07 18:04:03 -0300

    challenge 07

--- challenge07/index.js
+++ challenge07/index.js
@@ -1,9 +1,16 @@
+// 400 puntos si se spamea submit muchas veces
 function getGiftsToRefill(a1, a2, a3) {
-    return [...new Set(a1), ...new Set(a2), ...new Set(a3)].filter((item, index, array) => {
-        const removedItem = [...array]
-        removedItem.splice(index, 1)
-        return !removedItem.includes(item)
-    })
+    return [...new Set([...a1, ...a2, ...a3])].filter(e => a1.includes(e) + a2.includes(e) + a3.includes(e) === 1);
 }
+
+// Prefiero este
+// 200 puntos
+// function getGiftsToRefill(a1, a2, a3) {
+//     return [...new Set(a1), ...new Set(a2), ...new Set(a3)].filter((item, index, array) => {
+//         const removedItem = [...array]
+//         removedItem.splice(index, 1)
+//         return !removedItem.includes(item)
+//     })
+// }
 
 module.exports = getGiftsToRefill
