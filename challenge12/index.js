07f7262b1283cb330bfbefdf535baaed3064acbd
whoknowsi <echosmania@gmail.com>
2022-12-12 19:07:32 -0300

    Challenge 12 - Added one-liner solution

--- challenge12/index.js
+++ challenge12/index.js
@@ -1,9 +1,12 @@
+// 400 puntos pasando 3000 ops/s - One-liner solution 
+function selectSleigh(distance, sleighs) { return [{ name: null }, ...sleighs.filter(x => (20 / x.consumption) >= distance)].at(-1).name }
+
 // 400 puntos pasando 3000 ops/s
-function selectSleigh(distance, sleighs) {
-    sleighs = sleighs.filter((x) => x.consumption * distance <= 20)
-    sleighs.unshift({ name: null })
-    return sleighs[sleighs.length - 1].name
-}
+// function selectSleigh(distance, sleighs) {
+//     sleighs = sleighs.filter((x) => x.consumption * distance <= 20)
+//     sleighs.unshift({ name: null })
+//     return sleighs[sleighs.length - 1].name
+// }
 
 // 360 puntos pasando 3000 ops/s
 // function selectSleigh(distance, sleighs) {
