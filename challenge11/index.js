5a6ef08edc3d4e886fe1af7534fb68b5faa2f0c3
whoknowsi <echosmania@gmail.com>
2022-12-11 21:51:19 -0300

    challenge 11 - Deleting extra Number()

--- challenge11/index.js
+++ challenge11/index.js
@@ -1,9 +1,9 @@
 // 260 puntos
 function getCompleted(part, total) {
     const partToSeconds = part.split(':')
-        .reduce((acc, curr, i) => acc + Number(curr) * 60 ** (2 - i), 0)
+        .reduce((acc, curr, i) => acc + curr * 60 ** (2 - i), 0)
     const totalToSeconds = total.split(':')
-        .reduce((acc, curr, i) => acc + Number(curr) * 60 ** (2 - i), 0)
+        .reduce((acc, curr, i) => acc + curr * 60 ** (2 - i), 0)
 
     let a = partToSeconds
     let b = totalToSeconds
@@ -23,9 +23,9 @@ function getCompleted(part, total) {
 //     const mcd = (a, b) => !b ? a : mcd(b, a % b)
 
 //     const partToSeconds = part.split(':')
-//         .reduce((acc, curr, i) => acc + Number(curr) * 60 ** (2 - i), 0)
+//         .reduce((acc, curr, i) => acc + curr * 60 ** (2 - i), 0)
 //     const totalToSeconds = total.split(':')
-//         .reduce((acc, curr, i) => acc + Number(curr) * 60 ** (2 - i), 0)
+//         .reduce((acc, curr, i) => acc + curr * 60 ** (2 - i), 0)
 
 //     const gcd = mcd(partToSeconds, totalToSeconds)
 
