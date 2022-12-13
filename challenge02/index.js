10915379e9699fc3164babacfc7b0c6f49815a8f
whoknowsi <echosmania@gmail.com>
2022-12-13 14:03:59 -0300

    Challenge 02 - Added new response

--- challenge02/index.js
+++ challenge02/index.js
@@ -1,8 +1,16 @@
-const countHours = (year, holidays) => {
-    return holidays.reduce((acc, current) => {
-        const day = new Date(`${current}/${year}`).getDay()
-        return (day > 0 && day < 6) ? acc + 2 : acc
-    }, 0)
+// 121 puntos
+function countHours(year, holidays) {
+    return holidays
+        .filter(diaMes => new Date(diaMes + "/" + year).getDay() % 6)
+        .length * 2
 }
+
+// 91 puntos, antes daba más y se podía usar arrow functions
+// const countHours = (year, holidays) => {
+//     return holidays.reduce((acc, current) => {
+//         const day = new Date(`${current}/${year}`).getDay()
+//         return (day > 0 && day < 6) ? acc + 2 : acc
+//     }, 0)
+// }
 
 module.exports = countHours
