45fb9556cf941bef5aa77be8d6c448effc3235b1
whoknowsi <echosmania@gmail.com>
2022-12-14 23:36:13 -0300

    Challenge 13 - change misspelling on test

--- challenge13/index.test.js
+++ challenge13/index.test.js
@@ -5,7 +5,7 @@ describe('Challenge 13', () => {
         expect(typeof getFilesToBackup(0, [0, 0])).toBe('object')
     })
 
-    test("Test #02 - expect getFilesToBackup(1546300800, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) type be [1,3]", () => {
+    test("Test #02 - expect getFilesToBackup(1546300800, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) to be [1,3]", () => {
         expect(getFilesToBackup(1546300800, [
             [1, 1546300800],
             [2, 1546300800],
@@ -18,7 +18,7 @@ describe('Challenge 13', () => {
         ])
     })
 
-    test("Test #03 - expect getFilesToBackup(1546300600, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) type be [1,2,3]", () => {
+    test("Test #03 - expect getFilesToBackup(1546300600, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) to be [1,2,3]", () => {
         expect(getFilesToBackup(1546300600, [
             [1, 1546300800],
             [2, 1546300800],
@@ -32,7 +32,7 @@ describe('Challenge 13', () => {
         ])
     })
 
-    test("Test #04 - expect getFilesToBackup(1556300600, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) type be []", () => {
+    test("Test #04 - expect getFilesToBackup(1556300600, [[ 1, 1546300800 ],[ 2, 1546300800 ],[ 1, 1546300900 ],[ 1, 1546301000 ],[ 3, 1546301100 ]]) to be []", () => {
         expect(getFilesToBackup(1556300600, [
             [1, 1546300800],
             [2, 1546300800],
@@ -42,7 +42,7 @@ describe('Challenge 13', () => {
         ])).toEqual([])
     })
 
-    test("Test #04 - expect getFilesToBackup(1556300600, []) type be []", () => {
+    test("Test #04 - expect getFilesToBackup(1556300600, []) to be []", () => {
         expect(getFilesToBackup(1556300600, [])).toEqual([])
     })
 })
