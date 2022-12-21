90dbe53be1e4a154056a9e64e846cedcba533a6d
whoknowsi <echosmania@gmail.com>
2022-12-21 09:30:12 -0300

    Challenge 19 - Added 400 points response

--- challenge19/index.js
+++ challenge19/index.js
@@ -1,8 +1,17 @@
+// 400 puntos pasando las 3000 ops/s
 function sortToys(toys, positions) {
-    return positions.reduce((acc, curr, i) => {
-        acc[curr] = toys[i]
-        return acc
-    }, []).slice(Math.min(...positions))
+    return toys
+        .sort((a, b) =>
+            positions[toys.indexOf(a)] - positions[toys.indexOf(b)]
+        )
 }
+
+// 300 puntos pasando las 2000 ops/s
+// function sortToys(toys, positions) {
+//     return positions.reduce((acc, curr, i) => {
+//         acc[curr] = toys[i]
+//         return acc
+//     }, []).slice(Math.min(...positions))
+// }
 
 module.exports = sortToys
