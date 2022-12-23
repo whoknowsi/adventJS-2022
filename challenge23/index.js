8339b666ccb2cff0b10b80a8c5caa5ce6ab6652b
whoknowsi <echosmania@gmail.com>
2022-12-23 18:24:29 -0300

    Challenge 23 - reduce cognitive complexity

--- challenge23/index.js
+++ challenge23/index.js
@@ -1,4 +1,12 @@
+// Complejidad cognitiva: 9
 function executeCommands(commands) {
+    const normalizeNumber = (number) => {
+        return number > 255
+            ? number - 256
+            : number < 0
+                ? number + 256 : number
+    }
+
     const dictionary = {
         MOV: (a, b, registers) => {
             const pos1 = Number(a.slice(1))
@@ -10,37 +18,30 @@ function executeCommands(commands) {
         ADD: (a, b, registers) => {
             const pos1 = Number(a.slice(1))
             const pos2 = Number(b.slice(1))
-            registers[pos1] =
-                registers[pos1] + registers[pos2] > 255
-                    ? registers[pos1] + registers[pos2] - 256
-                    : registers[pos1] + registers[pos2]
+            registers[pos1] = normalizeNumber(registers[pos1] + registers[pos2])
         },
         DEC: (a, _, registers) => {
             const pos1 = Number(a.slice(1))
-            registers[pos1] = registers[pos1] - 1 === -1
-                ? 255
-                : registers[pos1] - 1
+            registers[pos1] = normalizeNumber(registers[pos1] - 1)
         },
         INC: (a, _, registers) => {
             const pos1 = Number(a.slice(1))
-            registers[pos1] = registers[pos1] + 1 === 256
-                ? 0
-                : registers[pos1] + 1
+            registers[pos1] = normalizeNumber(registers[pos1] + 1)
         },
         JMP: (a, _, registers, commands, current) => {
-            if (registers[0] !== 0) {
-                commands.slice(a, current + 1).forEach(command => {
-                    const [action, args] = command.split(' ')
-                    const [pos1, pos2] = args.split(',')
-                    dictionary[action](
-                        pos1,
-                        pos2,
-                        registers,
-                        commands,
-                        commands.indexOf(command)
-                    )
-                })
-            }
+            if (registers[0] === 0) return
+
+            commands.slice(a, current + 1).forEach(command => {
+                const [action, args] = command.split(' ')
+                const [pos1, pos2] = args.split(',')
+                dictionary[action](
+                    pos1,
+                    pos2,
+                    registers,
+                    commands,
+                    commands.indexOf(command)
+                )
+            })
         }
     }
 
