d12b4be11357f04d652d252b31929a75cea63c75
whoknowsi <echosmania@gmail.com>
2022-12-25 03:51:55 -0300

    Challenge 01 - tests typos

--- challenge01/index.test.js
+++ challenge01/index.test.js
@@ -1,11 +1,11 @@
 const wrapping = require('./index.js')
 
 describe('Challenge 01', () => {
-    test('Test #01 - return type', () => {
+    test('Test #01 - expected return type', () => {
         expect(Array.isArray(wrapping([]))).toBe(true);
     })
-        
-    test('Test #02 - wrapping(gifts)', () => {
+
+    test('Test #02 - expected wrapping(gifts)', () => {
         const gifts = [
             'cat',
             'game',
@@ -15,26 +15,26 @@ describe('Challenge 01', () => {
             "*****\n*cat*\n*****",
             "******\n*game*\n******",
             "*******\n*socks*\n*******"
-          ]
-    
-        expect(wrapping(gifts)).toEqual(expectedResult)
-    }) 
+        ]
 
-    test('Test #03 - wrapping(["midu"])', () => {
+        expect(wrapping(gifts)).toEqual(expectedResult)
+    })
+
+    test('Test #03 - expected wrapping(["midu"])', () => {
         const midu = ["midu"]
         const expectedResult = ["******\n*midu*\n******"]
 
         expect(wrapping(midu)).toEqual(expectedResult)
     })
 
-    test('Test #04 - wrapping(["a"])', () => {
+    test('Test #04 - expected wrapping(["a"])', () => {
         const a = ["a"]
         const expectedResult = ["***\n*a*\n***"]
 
         expect(wrapping(a)).toEqual(expectedResult)
     })
 
-    test('Test #05 - wrapping an empty array should return an empty array', () => {
+    test('Test #05 - expected wrapping an empty array should return an empty array', () => {
         expect(wrapping([])).toEqual([])
     })
 })
